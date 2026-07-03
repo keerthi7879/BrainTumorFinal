e20de6259f51ef32d8c30dca46ebc8c76d5e716c
keerthi7879 <keerthigak021@gmail.com>
2026-07-04 00:12:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,11 @@
-from flask import Flask, render_template, request
-import tensorflow as tf
-from tensorflow.keras.preprocessing import image
-import numpy as np
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Google Drive BASE_DIR configuration
-BASE_DIR = "/content/drive/MyDrive/BrainTumorFinal"
-
-# Models disabled temporarily to prevent GitHub deployment crash
-model_cnn = None
-model_vgg = None
-model_resnet = None
-
-
-def predict_image(img_path):
-    # Temporary safe return statement to prevent system crash
-    return "Model Not Available", 0.0
-
-
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def home():
+    return render_template("index.html")
 
-    result = None
-    confidence = None
-
-    if request.method == "POST":
-
-        file = request.files["file"]
-
-        # Automatically creating upload directory if missing
-        os.makedirs(os.path.join(BASE_DIR, "uploads"), exist_ok=True)
-
-        save_path = os.path.join(
-            BASE_DIR,
-            "uploads",
-            file.filename
-        )
-
-        file.save(save_path)
-
-        result, confidence = (
-            predict_image(save_path)
-        )
-
-    return render_template(
-        "index.html",
-        result=result,
-        confidence=confidence
-    )
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__=="__main__":
+    app.run(debug=True)
 
