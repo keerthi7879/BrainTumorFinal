c6fb7d41ec5ea19497d279d7ff74048de308d23e
keerthi7879 <keerthigak021@gmail.com>
2026-07-04 01:17:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 from flask import Flask, render_template
+import os
 import tensorflow as tf
 
 app = Flask(__name__)
 
-# Load models from local models folder
-model_cnn = tf.keras.models.load_model("models/custom_cnn_model.h5")
-model_vgg = tf.keras.models.load_model("models/vgg16_model.h5")
-model_resnet = tf.keras.models.load_model("models/resnet50_model.h5")
+model_cnn = None
+model_vgg = None
+model_resnet = None
+
+if os.path.exists("models/custom_cnn_model.h5"):
+    model_cnn = tf.keras.models.load_model(
+        "models/custom_cnn_model.h5"
+    )
+
+if os.path.exists("models/vgg16_model.h5"):
+    model_vgg = tf.keras.models.load_model(
+        "models/vgg16_model.h5"
+    )
+
+if os.path.exists("models/resnet50_model.h5"):
+    model_resnet = tf.keras.models.load_model(
+        "models/resnet50_model.h5"
+    )
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-if __name__ == "__main__":
-    app.run()
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=5000)
